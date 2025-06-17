648889667e22cad617c07a4acedf615d02e8e17d
evgeny.zarutsky <evgeny.zarutsky@gmail.com>
2025-06-17 20:51:12 +0300

    DI, IoC, Beans, BeanFactory, ApplicationContext [#505289]

--- src/main/java/ru/job4j/logic/AchievementService.java
+++ src/main/java/ru/job4j/logic/AchievementService.java
@@ -1,4 +1,7 @@
 package ru.job4j.logic;
 
+import org.jvnet.hk2.annotations.Service;
+
+@Service
 public class AchievementService {
 }
--- src/main/java/ru/job4j/telegramm/TelegramBotService.java
+++ src/main/java/ru/job4j/telegramm/TelegramBotService.java
@@ -1,4 +1,7 @@
 package ru.job4j.telegramm;
 
+import org.jvnet.hk2.annotations.Service;
+
+@Service
 public class TelegramBotService {
 }
