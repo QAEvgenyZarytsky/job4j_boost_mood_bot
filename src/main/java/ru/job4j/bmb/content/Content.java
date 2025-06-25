6fc8ce0c7a285104e378a4181ca74d5601aeb504
evgeny.zarutsky <evgeny.zarutsky@gmail.com>
2025-06-26 01:45:15 +0300

    Внедрение коллекций | Поставщики контекста [#505300]

--- src/main/java/ru/job4j/bmb/content/Content.java
+++ src/main/java/ru/job4j/bmb/content/Content.java
@@ -1,4 +1,63 @@
 package ru.job4j.bmb.content;
 
+import org.telegram.telegrambots.meta.api.objects.InputFile;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+
 public class Content {
+    private final Long chatId;
+    private String text;
+    private InputFile photo;
+    private InlineKeyboardMarkup markup;
+    private InputFile audio;
+
+    public Content(Long chatId) {
+        this.chatId = chatId;
+    }
+
+    public InputFile getAudio() {
+        return audio;
+    }
+
+    public void setAudio(InputFile audio) {
+        this.audio = audio;
+    }
+
+    public Long getChatId() {
+        return chatId;
+    }
+
+    public InlineKeyboardMarkup getMarkup() {
+        return markup;
+    }
+
+    public void setMarkup(InlineKeyboardMarkup markup) {
+        this.markup = markup;
+    }
+
+    public InputFile getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(InputFile photo) {
+        this.photo = photo;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return "Content{" +
+                "audio=" + audio +
+                ", chatId=" + chatId +
+                ", text='" + text + '\'' +
+                ", photo=" + photo +
+                ", markup=" + markup +
+                '}';
+    }
 }
